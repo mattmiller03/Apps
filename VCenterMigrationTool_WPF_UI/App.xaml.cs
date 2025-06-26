cadeeada726d8de58bfad0bae40e0793dddb1864
mattmiller03 <44274118+mattmiller03@users.noreply.github.com>
2025-06-26 06:39:41 -0400

    mvvm model updates

--- VCenterMigrationTool_WPF_UI/App.xaml.cs
+++ VCenterMigrationTool_WPF_UI/App.xaml.cs
@@ -1,14 +1,30 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using VCenterMigrationTool_WPF_UI.Infrastructure;
+using VCenterMigrationTool_WPF_UI.Utilities;
+using VCenterMigrationTool_WPF_UI.ViewModels;
 
 namespace VCenterMigrationTool_WPF_UI
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        public IServiceProvider Services { get; private set; } = null!;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var sc = new ServiceCollection();
+            sc.AddSingleton<Logger>();
+            sc.AddSingleton<PowerShellManager>();
+            sc.AddSingleton<MainViewModel>();
+            Services = sc.BuildServiceProvider();
+
+            var wnd = new MainWindow
+            {
+                DataContext = Services.GetRequiredService<MainViewModel>()
+            };
+            wnd.Show();
+            base.OnStartup(e);
+        }
+    }
 }
---
+++ VCenterMigrationTool_WPF_UI/Infrastructure/Logger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VCenterMigrationTool_WPF_UI.Infrastructure
+{
+    public sealed class Logger
+    {
+        public event Action<string, string>? MessageWritten;
+        public void Info(string m) => MessageWritten?.Invoke(m, "INFO");
+        public void Warn(string m) => MessageWritten?.Invoke(m, "WARN");
+        public void Error(string m) => MessageWritten?.Invoke(m, "ERR ");
+    }
+}
+
